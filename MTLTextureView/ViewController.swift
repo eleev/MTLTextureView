3b5c04ca0b10d5fccbaeca0ebd6575e0874cb7cf
Astemir Eleev <astemireleev@gmail.com>
2023-04-02 16:13:08 -0700

    Initial Commit

---
+++ MTLTextureView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MTLTextureView
+//
+//  Created by Astemir Eleev on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
